cb88ef396ee3804bdd7d80cdf0bc65f1a16fff49
Philip Skaria <philipskaria04@gmail.com>
2025-09-20 16:21:48 +0530

    Added accountSchema for account balance

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,54 @@
-const mongoose=require("mongoose")
-mongoose.connect("mongodb://localhost:27017/paytm");
+// backend/db.js
+const mongoose = require('mongoose');
 
-const mongooseSchema=mongoose.Schema({
-    username:String,
-    password:String,
-    firstName:String,
-    lastName:String,
-})
+mongoose.connect("mongodb://localhost:27017/paytm")
 
-const User=mongoose.model("User",userSchema);
+// Create a Schema for Users
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }
+});
+// schema for account balance
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User',// Reference to User model
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+});
 
-module.exports={
-    User
-}
+const Account = mongoose.model('Account', accountSchema);
+const User = mongoose.model('User', userSchema);
+
+module.exports = {
+	User,
+  Account,
+};
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -7,43 +7,84 @@ const { authMiddleware } = require("../middleware");
 const router=express.Router();
 
 //user signup
-const signupSchema=zod.object({
-    username:zod.string(),
-    password:zod.string(),
-    firstName:zod.string(),
-    lastName:zod.string()
+const signupBody = zod.object({
+    username: zod.string().email(),
+	firstName: zod.string(),
+	lastName: zod.string(),
+	password: zod.string()
 })
 
-router.post("/signup",async(req,res)=>{
-    const body=req.body;
-    const {success}=signupSchema.safeParse(req.body);
-    if(!success){
-        return res.json({
-            message:"Email already taken / Incorrect inputs"
+router.post("/signup", async (req, res) => {
+    const { success } = signupBody.safeParse(req.body)
+    if (!success) {
+        return res.status(411).json({
+            message: "Email already taken / Incorrect inputs"
         })
     }
 
-    const user=User.findOne({
-        username:body.username
+    const existingUser = await User.findOne({
+        username: req.body.username
     })
 
-    if(user._id){
-        return res.json({
-            message:"Email already taken / Incorrect inputs"
+    if (existingUser) {
+        return res.status(411).json({
+            message: "Email already taken/Incorrect inputs"
         })
     }
 
-    const dbUser=await User.create(body);
-    const token=jwt.sign({
-        userId:dbUser._id
-    },JWT_SECRET);
+    const user = await User.create({
+        username: req.body.username,
+        password: req.body.password,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+    })
+    const userId = user._id;
+
+    const token = jwt.sign({
+        userId
+    }, JWT_SECRET);
 
     res.json({
-        message:"User created successfully",
-        token:token
+        message: "User created successfully",
+        token: token
     })
-
 })
+
+const signinBody = zod.object({
+    username: zod.string().email(),
+	password: zod.string()
+})
+
+router.post("/signin", async (req, res) => {
+    const { success } = signinBody.safeParse(req.body)
+    if (!success) {
+        return res.status(411).json({
+            message: "Email already taken / Incorrect inputs"
+        })
+    }
+
+    const user = await User.findOne({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    if (user) {
+        const token = jwt.sign({
+            userId: user._id
+        }, JWT_SECRET);
+  
+        res.json({
+            token: token
+        })
+        return;
+    }
+
+    
+    res.status(411).json({
+        message: "Error while logging in"
+    })
+})
+
 
 
 const updateBody = zod.object({
