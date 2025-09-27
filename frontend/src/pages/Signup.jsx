fc9145dd3e9a3ee333b13c65808d7a5d5a3289ac
Philip Skaria <philipskaria04@gmail.com>
2025-09-27 18:39:01 +0530

    navigate to dashboard on auth success

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -5,12 +5,14 @@ import { Button } from "../components/Button"
 import { Heading } from "../components/Heading"
 import { InputBox } from "../components/InputBox"
 import { SubHeading } from "../components/SubHeading"
+import { useNavigate } from "react-router-dom";
 
 export const Signup=()=>{
     const [firstName,setFirstName]=useState("");
     const [lastName,setLastName]=useState("");
     const [userName,setUserName]=useState("");
     const [password,setPassword]=useState("");
+    const navigate=useNavigate();
     return(
         <div className="bg-slate-300 h-screen flex justify-center">
             <div className="flex flex-col justify-center">
@@ -38,7 +40,7 @@ export const Signup=()=>{
                                 password
                             })
                             localStorage.setItem("token",response.data.token)
-                            localStorage.removeItem("token")
+                            navigate("/dashboard")
                         }}label={"Sign Up"}/>
                     </div>
                     <BottomWarning label={"Already have an account?"} buttonText={"Sign in"} to={"/signin"} />
